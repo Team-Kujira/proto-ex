d289b94e61edb51267b73c8ae6352a00306d5d0e
codehans <94654388+codehans@users.noreply.github.com>
2024-01-23 11:20:05 +0000

    docs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule KujiraProto.MixProject do
       version: "0.9.3",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -32,7 +33,8 @@ defmodule KujiraProto.MixProject do
     [
       {:protobuf, "~> 0.10.0"},
       {:google_protos, "~> 0.1"},
-      {:grpc, "~> 0.5.0"}
+      {:grpc, "~> 0.5.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
